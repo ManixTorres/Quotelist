bafb230d21c86500a241377e0aa28fd8fa21681b
ManixTorres <51294779+ManixTorres@users.noreply.github.com>
2021-01-08 12:55:27 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+package stuff;
+
+import java.util.Scanner;
+
+public class stuff{
+    public static void main(String[] args){
+
+    String[] quotes = {"One Has Many Faces", "The Moon Shines Bright", "Peace Comes After a War", "Trust No One", "Fight", "Bad Things are Bad", "Good Things are Good", "There's no I in team", "Darkness within Darkness await those who seek it", "Feel The Emptiness, as we are alone for most of our lives"}
+
+}
